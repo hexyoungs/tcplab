851b4c0ca742e2f9d283115c431f5f23a21b4037
hexyoungs <hexyoungs@outlook.com>
2020-08-28 18:24:26 +0800

    lab1: add answer

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -12,15 +12,52 @@ void DUMMY_CODE(Targs &&... /* unused */) {}
 
 using namespace std;
 
-StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity), _capacity(capacity) {}
+StreamReassembler::StreamReassembler(const size_t capacity)
+    : _chunks(), _output(capacity), _capacity(capacity), _pos(0), _max_pos(0), _eof(false) {
+    make_heap(_chunks.begin(), _chunks.end(), DataChunkCmp());
+}
 
 //! \details This function accepts a substring (aka a segment) of bytes,
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    if (_eof)
+        return;
+    if (_max_pos < index + data.length())
+        _max_pos = index + data.length();
+    DataChunk chunk = DataChunk{data, index, eof};
+    _chunks.push_back(chunk);
+    push_heap(_chunks.begin(), _chunks.end(), DataChunkCmp());
+
+    while (!_chunks.empty() && !_eof) {
+        DataChunk c = _chunks.front();
+
+        if (c.index < _pos) {
+            // duplicated packet
+            // or overlapped packet
+            int extra = c.data.length() + c.index - _pos;
+            if (extra > 0) {
+                // more data to write
+                _output.write(c.data.substr(c.data.length() - extra));
+                _pos += extra;
+            }
+        } else if (c.index == _pos) {
+            _output.write(c.data);
+            _pos += c.data.length();
+        } else {
+            // should wait more
+            break;
+        }
+
+        pop_heap(_chunks.begin(), _chunks.end(), DataChunkCmp());
+        _chunks.pop_back();
+        if (c.eof) {
+            _eof = true;
+            _output.end_input();
+        }
+    }
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const { return _pos == 0 ? (_max_pos - 1) : (_max_pos - _pos); }
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const { return _pos == 0; }
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -3,17 +3,31 @@
 
 #include "byte_stream.hh"
 
+#include <algorithm>
 #include <cstdint>
 #include <string>
+#include <vector>
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
 class StreamReassembler {
   private:
-    // Your code here -- add private members as necessary.
+    struct DataChunk {
+        std::string data;
+        std::uint64_t index;
+        bool eof;
+    };
+    struct DataChunkCmp {
+        bool operator()(const DataChunk &a, const DataChunk &b) { return a.index > b.index; }
+    };
+
+    std::vector<DataChunk> _chunks;
 
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
+    size_t _pos;
+    size_t _max_pos;
+    bool _eof;
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
